436b85d864774dffd37ae9fcbee085957a71ddff
Justin De Sio <desio.j@live.fr>
2026-03-11 19:29:04 +0100

    Initial Commit

---
+++ IOS cam/IOS_camApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_camApp.swift
+//  IOS cam
+//
+//  Created by Justin De Sio on 11/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct IOS_camApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
